dfd26188531766e19ecc70f84fe0e37777c5b773
H1Ash <112007774+H1Ash@users.noreply.github.com>
2023-03-19 02:19:13 +0530

    Final Commit

--- expage.py
+++ expage.py
@@ -5,50 +5,52 @@ import seaborn as sns
 from PIL import Image
 
 
-def shorten_categories(cat, cutoff):
-    categorial_map = {}
-    for i in range(len(cat)):
-        if cat.values[i] >= cutoff:
-            categorial_map[cat.index[i]] = cat.index[i]
-        else:
-            categorial_map[cat.index[i]] = "other"
-    return categorial_map
+# def shorten_categories(cat, cutoff):
+#     categorial_map = {}
+#     for i in range(len(cat)):
+#         if cat.values[i] >= cutoff:
+#             categorial_map[cat.index[i]] = cat.index[i]
+#         else:
+#             categorial_map[cat.index[i]] = "other"
+#     return categorial_map
 
 
-def clean_exp(x):
-    if x == "Less than 1 year":
-        return 0.5
-    if x == "More than 50 years":
-        return 50
-    return float(x)
+# def clean_exp(x):
+#     if x == "Less than 1 year":
+#         return 0.5
+#     if x == "More than 50 years":
+#         return 50
+#     return float(x)
 
 
-def clean_ed(x):
-    if 'Bachelor’s degree' in x:
-        return 'Bachelor'
-    if "Master’s degree" in x:
-        return 'Masters'
-    if 'Professional degree' in x or 'Other doctoral degree' in x:
-        return 'PostGrad'
-    return "less than bachelor's"
+# def clean_ed(x):
+#     if 'Bachelor’s degree' in x:
+#         return 'Bachelor'
+#     if "Master’s degree" in x:
+#         return 'Masters'
+#     if 'Professional degree' in x or 'Other doctoral degree' in x:
+#         return 'PostGrad'
+#     return "less than bachelor's"
 
 @st.cache_data
 def load_data():
-    df = pd.read_csv("survey_results_public.csv")
-    df = df[["Country", "EdLevel", "YearsCodePro", "Employment", "ConvertedCompYearly"]]
-    df = df.rename({"ConvertedCompYearly": "Salary"}, axis=1)
-    df = df.rename({"YearsCodePro": "CodingExperiance"}, axis=1)
-    df = df[df.Salary.notnull()]
-    df = df.dropna()
-    df = df[df["Employment"] == "Employed, full-time"]
-    Country_map = shorten_categories(df.Country.value_counts(), 500)
-    df.Country = df.Country.map(Country_map)
-    df = df[df.Salary <= 175000]
-    df = df[df.Salary >= 10000]
-    df = df[df.Country != "other"]
-    df = df.drop("Employment", axis=1)
-    df.EdLevel = df.EdLevel.apply(clean_ed)
-    df.CodingExperiance = df.CodingExperiance.apply(clean_exp)
+    df = pd.read_csv("cleaned_data.csv")
+    # df = pd.read_csv("survey_results_public.csv")
+    # df = df[["Country", "EdLevel", "YearsCodePro", "Employment", "ConvertedCompYearly"]]
+    # df = df.rename({"ConvertedCompYearly": "Salary"}, axis=1)
+    # df = df.rename({"YearsCodePro": "CodingExperiance"}, axis=1)
+    # df = df[df.Salary.notnull()]
+    # df = df.dropna()
+    # df = df[df["Employment"] == "Employed, full-time"]
+    # Country_map = shorten_categories(df.Country.value_counts(), 500)
+    # df.Country = df.Country.map(Country_map)
+    # df = df[df.Salary <= 175000]
+    # df = df[df.Salary >= 10000]
+    # df = df[df.Country != "other"]
+    # df = df.drop("Employment", axis=1)
+    # df.EdLevel = df.EdLevel.apply(clean_ed)
+    # df.CodingExperiance = df.CodingExperiance.apply(clean_exp)
+    # df.to_csv('cleaned_data.csv')
     return df
 
 df = load_data()
